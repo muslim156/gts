4f2e3fc287c41be653210ee1a217ff29f9d2c09e
Haris Osmanagić <haris.osmanagic@gmail.com>
2012-11-14 10:19:37 +0100

    dodao podrsku za citanje UTF-8 karaktera. izbacio probne podatke iz sablona.

--- src/main/java/ba/kickboxing/draw/persistence/IO.java
+++ src/main/java/ba/kickboxing/draw/persistence/IO.java
@@ -2,9 +2,11 @@ package ba.kickboxing.draw.persistence;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -74,8 +76,9 @@ public class IO {
 	public static List<Player> readFromTxt(String filePath) throws IOException {
 		List<Player> players = new ArrayList<Player>();
 
-		BufferedReader reader = new BufferedReader(new FileReader(new File(
-				filePath)));
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				new FileInputStream(filePath), "UTF8"));
+		
 
 		Player player = null;
 
